fdc5260d0147ebe9182d26bb1d22dbec7902ba7f
Shrirang Lokhande <shrirangrlokhande@gmail.com>
2025-05-08 01:57:48 +0100

    Adding documentation for Health.js

--- docs/src/items/Health.js
+++ docs/src/items/Health.js
@@ -1,8 +1,21 @@
+/**
+ * Health class models the health property associated with a player.
+ * This is one of the twist elements of our game where player's health deteriorate
+ * over time, incentivising the player to navigate obstacles and reach the cave.
+ * Health is displayed using a bar on top left of the screen.
+ */
+
 // Constructor, update, and display methods
 class Health {
+  //Default maximum health
   static initialPercentage = 1;
+  //Default reduction rate for health.
   static reductionRate = 0.00032;
 
+  /**
+   * Constructor initializes the graphic attributes corresponding to the health i.e. health bar.
+   * @param assetManager - Reference of asset manager.
+   */
   constructor(assetManager) {
     this.assetManager = assetManager;
     // Initialize the health bar
@@ -14,20 +27,35 @@ class Health {
     this.y = 25;
   }
 
+  /**
+   * Updates the health before each new frame of the game according to set reduction rate.
+   */
   updateHealth() {
     if (this.percentage > 0) {
       this.percentage = this.percentage - Health.reductionRate;
     }
   }
 
+  /**
+   * Getter for health percentage
+   * @returns {number} - Health percentage
+   */
   getHealth() {
     return this.percentage;
   }
 
+  /**
+   * Setter for health percentage
+   * @param percentage - Input health percentage.
+   */
   setHealth(percentage) {
     this.percentage = percentage;
   }
 
+  /**
+   * Displays the health using a health bar using appropriate visual resources
+   * from asset manager on the top left side of the game screen.
+   */
   display() {
     // Draw the rectangle with the top right corner point
     rectMode(CORNER);
