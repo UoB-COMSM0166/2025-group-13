e4bfe4e698a15fc83d065fbc9f1ab4adffba363c
Shrirang Lokhande <shrirangrlokhande@gmail.com>
2025-05-08 12:54:22 +0100

    Adding the documentation for Game.js

--- docs/src/Game.js
+++ docs/src/Game.js
@@ -1,6 +1,15 @@
-// Game class handles the main game logic and delegate jobs through each independent objects
+/**
+ * Game class handles the main game logic and delegate jobs through each independent objects.
+ * Game class keep track of levels, draws game map based on it. passess asset manager reference to 
+ * other class instances. Updates and draws player and the map.
+ */
 
 class Game {
+    /**
+     * Constructor initializes the game object which remains in memory until user closes the window.
+     * @param gameLevel - index of level to be played at the start.
+     * @param assetManager - asset manager reference required to retrieve visual & audio data.
+     */
     constructor(gameLevel, assetManager) {
         this.currentLevel = gameLevel;
         this.assetManager = assetManager;
@@ -32,6 +41,9 @@ class Game {
         this.stopMapMovement = false;
     }
 
+    /**
+     * Sets up the game map layout with data for current level and audio effect .
+     */
     setup() {
         this.map.setup(layouts[this.currentMap]); //init map
         // dino noisy - everytime start a level
@@ -39,12 +51,18 @@ class Game {
         this.assetManager.effect_dino.play();
     }
 
+    /**
+     * Updates the game based on player interactions with the map objects.
+     */
     update() {
         this.map.update(); // display map
         this.player.update(this.map.platforms, this.map.foods, this.map.groundDamages, this.map.enemies, this.map.skyFalls, this.map.cave);
         this.health.updateHealth();
     }
 
+    /**
+     * Draws the game screen in its entirety i.e. the background, player, obstacles, NPCs and food items etc.
+     */
     draw() {
         if (this.assetManager.gamePageBackground) {
             switch (this.currentMap) {
@@ -109,11 +127,21 @@ class Game {
         this.update();
     }
 
+    /**
+     * Handles input proved by the user via touch screen buttons or keyboard
+     * @param triggerJump - True if jump key/button is provided.
+     * @param moveLeft - True if left key/button is pressed.
+     * @param moveRight - True if right key/button is pressed.
+     */
     handleInput(triggerJump, moveLeft, moveRight) {
         this.map.handleInput(moveRight);
         this.player.handleInput(moveLeft, moveRight, triggerJump);
     }
 
+    /**
+     * Checks if the player has died while gameplay.
+     * @returns {boolean}
+     */
     isGameOver() {
         if (this.health.getHealth() <= 0) {
             return true;
@@ -121,11 +149,20 @@ class Game {
         else return false;
     }
 
+    /**
+     * Changes the game level and load the new map.
+     * After last level the next level is the default first level.
+     */
     nextLevel() {
         this.currentMap = (this.currentMap + 1) % this.maps.length;
         this.map = this.maps[this.currentMap];
     }
 
+    /**
+     * Checks if a level has been completed by the player by reaching the cave.
+     * Sets the appropriate sound effect for level completion.
+     * @returns {boolean} - True if level has been completed else false.
+     */
     isLevelComplete() {
         if (this.player.reachedCave) {
             if (this.assetManager.bgm_exciting.isPlaying()) {
